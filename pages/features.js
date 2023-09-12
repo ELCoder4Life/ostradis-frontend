d6f6d8ec5434989d5ce6fc299373b63af3f17b7f
Manoj Ahirwar <manojahirwar11@gmail.com>
2023-09-12 16:04:30 +0530

    pricing and feature

--- pages/features.js
+++ pages/features.js
@@ -1,24 +1,13 @@
 import LandingLayout from "@/components/landingPage/landingLayout";
-import SEO from "@/components/seo";
 import LandingHeader from "@/components/landingPage/landingHeader";
 import LandingFeature from "@/components/landingPage/landingFeature";
-import LandingHowTo from "@/components/landingPage/landingHowTo";
 import LandingHero from "@/components/landingPage/landingHero";
-import LandingStats from "@/components/landingPage/landingStats";
-import LandingPricing from "@/components/landingPage/landingPricing";
 import LandingFooter from "@/components/landingPage/landingFooter";
 import LandingCta from "@/components/landingPage/landingCta";
 import { useEffect } from "react";
+import SEO from "@/components/additional/seo";
 
-export function Home() {}
-
-export async function getServerSideProps(context) {
-  return {
-    props: { show_user_page: false },
-  };
-}
-
-export default function PageWrapper(props) {
+export default function Feature() {
   useEffect(() => {
     const pricingElement = document.getElementById("features");
     if (pricingElement) {
@@ -27,9 +16,8 @@ export default function PageWrapper(props) {
   }, []);
   return (
     <>
-      {/* <LandingPage {...props} /> */}
       <LandingLayout>
-        <SEO title="PagePe.com - Features" />
+        <SEO />
         <LandingHeader isDashboard={true} from="index" navLinkEnable={true} />
         <main className="space-y-20 mb-0">
           <LandingHero />
--- pages/pricing.js
+++ pages/pricing.js
@@ -1,24 +1,13 @@
 import LandingLayout from "@/components/landingPage/landingLayout";
-import SEO from "@/components/seo";
 import LandingHeader from "@/components/landingPage/landingHeader";
-import LandingFeature from "@/components/landingPage/landingFeature";
-import LandingHowTo from "@/components/landingPage/landingHowTo";
 import LandingHero from "@/components/landingPage/landingHero";
-import LandingStats from "@/components/landingPage/landingStats";
 import LandingPricing from "@/components/landingPage/landingPricing";
 import LandingFooter from "@/components/landingPage/landingFooter";
 import LandingCta from "@/components/landingPage/landingCta";
 import { useEffect } from "react";
+import SEO from "@/components/additional/seo";
 
-export function Home() {}
-
-export async function getServerSideProps(context) {
-  return {
-    props: { show_user_page: false },
-  };
-}
-
-export default function PageWrapper(props) {
+export default function Pricing() {
   useEffect(() => {
     const pricingElement = document.getElementById("pricing");
     if (pricingElement) {
@@ -27,10 +16,9 @@ export default function PageWrapper(props) {
   }, []);
   return (
     <>
-      {/* <LandingPage {...props} /> */}
       <LandingLayout>
-        <SEO title="PagePe.com - Pricing" />
-        <LandingHeader isDashboard={true} from="index" navLinkEnable={true} />
+        <SEO />
+        <LandingHeader />
         <main className="space-y-20 mb-0">
           <LandingHero />
           <LandingPricing />
