c160630cdef473b28c80c18812615705b3e3bf13
Manoj Ahirwar <manojahirwar11@gmail.com>
2023-09-15 14:22:02 +0530

    chatbot

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,6 +3,7 @@ import LandingHeader from "@/components/landingPage/landingHeader";
 import SEO from "@/components/additional/seo";
 import DashboardHero from "@/components/dashboard/DashboardHero";
 import FeedDataCard from "@/components/aiComponent/feedDataCard";
+import Chatbot from "@/components/aiComponent/chatbot";
 
 export default function Dashboard() {
   return (
@@ -13,6 +14,7 @@ export default function Dashboard() {
         <main className="space-y-40 mb-0">
           <DashboardHero />
           <FeedDataCard />
+          <Chatbot />
         </main>
       </LandingLayout>
     </>
